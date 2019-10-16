a9d330ee3ebaf5a7696b4ab0d076af8ff984c4e5
Liviu Preda <liviupreda@gmail.com>
2019-10-16 11:22:58 +0300

    create folder structure, refactor code, build production files

---
+++ docs/precache-manifest.e1ee38da323d54e18b58f14f88f3622d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ca30456e0cdc5ff708d34c57115cfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7de5d036913d526fd145",
+    "url": "/static/css/main.4a90a3ac.chunk.css"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/static/js/2.f9b2e496.chunk.js"
+  },
+  {
+    "revision": "7de5d036913d526fd145",
+    "url": "/static/js/main.79b52d55.chunk.js"
+  },
+  {
+    "revision": "05efcc9d811b4a2a3c6e",
+    "url": "/static/js/runtime-main.8f8b62bf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
